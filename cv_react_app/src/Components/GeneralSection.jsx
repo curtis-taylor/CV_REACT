b9ab82c33eda372dd9dabdbb30646f3a54853037
curtis-taylor <curtis.t@gmail.com>
2026-05-10 23:18:03 -0400

    trying to create a section component

---
+++ cv_react_app/src/Components/GeneralSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export function GeneralSection() {
+    return (
+        <>
+        <h3>General Information</h3>
+        <div id="basic_textfields">
+            <label>Name: </label>
+            <input></input>
+            <label>Email: </label>
+            <input></input>
+            <label>Phone: </label>
+            <input></input>
+        </div>
+      </>
+
+    );
+}
