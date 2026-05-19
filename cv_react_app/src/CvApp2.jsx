1eb365aab75ee1a15fc9329c4f6f973911be1f65
curtis taylor <curtis.t@gmail.com>
2026-05-19 03:31:48 -0400

    add more input fields

--- cv_react_app/src/CvApp2.jsx
+++ cv_react_app/src/CvApp2.jsx
@@ -10,24 +10,66 @@ import './Components/Buu.jsx'
 function CvApp2() {
 
     const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [school, setSchool] = useState('')
+    const [major, setMajor] = useState('')
+    const [dateComplete, setDateComplete] = useState('')
+
+
     const [count, setCount] = useState(0)
     //const [cvText, setCVText] = useState("");
 
     const handleSubmit = (e) => {
     e.preventDefault();
-    alert(`Hello, ${name}`);
+    
+    info = [name, email, phone, school, major, dateComplete]
+    alert(`Hello, ${info}`);
   };
 
     return (
 
     <form onSubmit={handleSubmit}>
+      <div>
       <input
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
         placeholder="Enter your name"
       />
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter your email"
+      /> 
+      <input
+        type="text"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        placeholder="Enter your Phone Number"
+      /> 
+      <input
+        type="text"
+        value={school}
+        onChange={(e) => setSchool(e.target.value)}
+        placeholder="Enter your School Name"
+      /> 
+       <input
+        type="text"
+        value={major}
+        onChange={(e) => setMajor(e.target.value)}
+        placeholder="Enter your School Major"
+      /> 
+       <input
+        type="text"
+        value={dateComplete}
+        onChange={(e) => setDateComplete(e.target.value)}
+        placeholder="Enter completion date"
+      /> 
+      </div><br></br>
       <button type="submit">Submit</button>
+      <button type="submit">Edit</button>
     </form>
 
     )
