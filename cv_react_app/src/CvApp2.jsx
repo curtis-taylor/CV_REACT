87eacde1e2eb790c71f429ff2d57347a0d0249d9
curtis-taylor <curtis.t@gmail.com>
2026-05-19 23:03:57 -0400

    more form work

--- cv_react_app/src/CvApp2.jsx
+++ cv_react_app/src/CvApp2.jsx
@@ -23,51 +23,60 @@ function CvApp2() {
     const handleSubmit = (e) => {
     e.preventDefault();
     
-    info = [name, email, phone, school, major, dateComplete]
+    let info = [name, email, phone, school, major, dateComplete]
     alert(`Hello, ${info}`);
+
+    return (
+      <h1>TTTEYWTEYT</h1>
+    )
   };
 
     return (
 
     <form onSubmit={handleSubmit}>
       <div>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Enter your name"
-      />
-      <input
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Enter your email"
-      /> 
-      <input
-        type="text"
-        value={phone}
-        onChange={(e) => setPhone(e.target.value)}
-        placeholder="Enter your Phone Number"
-      /> 
-      <input
-        type="text"
-        value={school}
-        onChange={(e) => setSchool(e.target.value)}
-        placeholder="Enter your School Name"
-      /> 
-       <input
-        type="text"
-        value={major}
-        onChange={(e) => setMajor(e.target.value)}
-        placeholder="Enter your School Major"
-      /> 
-       <input
-        type="text"
-        value={dateComplete}
-        onChange={(e) => setDateComplete(e.target.value)}
-        placeholder="Enter completion date"
-      /> 
-      </div><br></br>
+        <div className='section'>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter your name"
+            />
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+            /> 
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              placeholder="Enter your Phone Number"
+            /> 
+        </div>
+        <div className='section'>
+            <input
+              type="text"
+              value={school}
+              onChange={(e) => setSchool(e.target.value)}
+              placeholder="Enter your School Name"
+            /> 
+            <input
+              type="text"
+              value={major}
+              onChange={(e) => setMajor(e.target.value)}
+              placeholder="Enter your School Major"
+            /> 
+            <input
+              type="text"
+              value={dateComplete}
+              onChange={(e) => setDateComplete(e.target.value)}
+              placeholder="Enter completion date"
+            /> 
+        </div>
+      </div>
+      <br></br>
       <button type="submit">Submit</button>
       <button type="submit">Edit</button>
     </form>
