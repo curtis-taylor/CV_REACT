9352a5894bc20b189884f46b11ae2d465e8f3091
curtis taylor <curtis.t@gmail.com>
2026-06-15 03:18:36 -0400

    Starting to Make CV app look more accurate

--- cv_react_app/src/Components/Form.jsx
+++ cv_react_app/src/Components/Form.jsx
@@ -17,6 +17,7 @@ export function Form() {
     const [dateComplete, setDateComplete] = useState('')
 
     const [company, setCompany] = useState('')
+    const [location, setCompanyLocation] = useState('')
     const [position, setPosition] = useState('')
     const [tasks, setTasks] = useState('')
     const [jobStart, setJobStart] = useState('');
@@ -28,7 +29,7 @@ export function Form() {
     const handleSubmit = (e) => {
       e.preventDefault();
       
-      let info = [ name, email, phone, school, major, dateComplete, company, position, tasks, jobStart]
+      let info = [ name, email, phone, school, major, dateComplete, company, position, tasks, jobStart, location]
       //alert(`Hello, ${info}`);
 
       setCVText(info);
@@ -107,6 +108,12 @@ export function Form() {
             /> 
             <input
               type="text"
+              value={location}
+              onChange={(e) => setCompanyLocation(e.target.value)}
+              placeholder="Enter Company Location City etc"
+            /> 
+            <input
+              type="text"
               value={position}
               onChange={(e) => setPosition(e.target.value)}
               placeholder="Enter Company position title"
@@ -126,6 +133,7 @@ export function Form() {
               onChange={(e) => setTasks(e.target.value)}
               placeholder="Enter position Responsibilities"
             /> 
+
         </div>
         
       </div>
@@ -161,8 +169,9 @@ export function Form() {
       <br></br><br></br>
 
       <div>
+         <div><h2><u>EDUCATION</u></h2></div>
          <div>
-            <span> <b>School Name: </b> {cvText[3]} </span>
+            <span> <b>School Name: </b> <i> {cvText[3]} </i> </span>
         </div>
         <div>
             <span> <b>Major:</b> {cvText[4]} </span>
@@ -176,16 +185,14 @@ export function Form() {
       <br></br><br></br>
 
       <div>
+         <div><h2><u>EXPERIENCE</u></h2></div>
          <div>
-            <span> <b>Company Name:</b> {cvText[6]} </span>
+            <span> <b>{cvText[7]} </b></span>
+        </div>
+         <div>
+            <span> <i>{cvText[6]}</i>, {cvText[10]} ({cvText[9]}) </span>
         </div>
         
-        <div>
-            <span> <b>Position Title:</b> {cvText[7]} </span>
-        </div>
-        <div>
-            <span> <b>Start Date:</b> {cvText[9]} </span>
-        </div>
         <div>
             <span> <b>Position Tasks and Responsibilities:</b> {cvText[8]} </span>
         </div>
